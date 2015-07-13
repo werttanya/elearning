19c0ebd5994d4e9b639a54b7753ad41516219aaa
tanya <werttanya@gmail.com>
2015-07-13 20:31:02 +0200

    statistics modification

--- elearning/elearning/core/courses/views.py
+++ elearning/elearning/core/courses/views.py
@@ -5,10 +5,12 @@ from elearning.settings import REST_API, PYGAL_JS_FILES
 from django.http import HttpResponseBadRequest, HttpResponse
 from django.core.servers.basehttp import FileWrapper
 from django.core.urlresolvers import reverse
+from elearning.core.errors.views import server_error
 from .forms import CourseForm
 from datetime import datetime
 from pygal.style import Style
 from pygal.colors import darken
+from pygal.config import Config
 import json
 import pygal
 import requests
@@ -35,7 +37,7 @@ question_style = Style(
     opacity='.5',
     opacity_hover='.9',
     transition='250ms ease-in',
-    colors = ('#00b2f0', '#43d9be', '#0662ab', darken('#00b2f0', 20)))
+    colors = ('#0ACF00','#0E51A7', '#FD0006', '#FF9E00'))
 
 motivation_style = Style(
     background=darken('#f8f8f8', 3),
@@ -77,7 +79,11 @@ def courses(request):
     email = request.session['email']
     password = request.session['password']
     r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
-    courses = r.json(object_hook=json_object_hook)
+    if r.status_code == requests.codes.ok:
+        courses = r.json(object_hook=json_object_hook)
+    else:
+        courses = []
+        return server_error(request)
     paginator = Paginator(courses, 4)
     try:
         courses = paginator.page(page)
@@ -86,8 +92,7 @@ def courses(request):
     except EmptyPage:
         courses = paginator.page(paginator.num_pages)
     return render(request, 'courses/courses.html', {
-        'courses': courses, 'paginator': paginator
-    })
+        'courses': courses, 'paginator': paginator })
 
 
 def add_course(request):
@@ -106,7 +111,7 @@ def add_course(request):
                 courses_url = '/courses/'
                 return redirect(courses_url)
             else:
-                return HttpResponseBadRequest()
+                return server_error(request)
     form = CourseForm()
     return render(request, 'courses/add_course.html', {'form': form,})
 
@@ -116,9 +121,13 @@ def course_page(request, course_id):
     email = request.session['email']
     password = request.session['password']
     r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
+    if r.status_code != requests.codes.ok:
+        return server_error(request)
     course = r.json(object_hook=json_object_hook)
     quizzes_url = '/quizzes?course_id={0}'.format(course_id)
     qr = requests.get(REST_API+quizzes_url, headers=accept_json_header, auth=(email, password))
+    if qr.status_code != requests.codes.ok:
+        return server_error(request)
     quizzes = qr.json(object_hook=json_object_hook)
     quizzes_dates = []
     for quiz in quizzes:
@@ -161,7 +170,7 @@ def add_quiz(request, course_id):
         if r.status_code == requests.codes.created:
             return redirect(reverse('course_page', args=[course_id]))
         else:
-            return HttpResponseBadRequest()
+            return server_error(request)
     return render(request, 'courses/add_quiz.html')
 
 def publish_quiz(request, course_id, quiz_id):
@@ -193,12 +202,22 @@ def quiz_page(request, course_id, quiz_id):
     email = request.session['email']
     password = request.session['password']
     r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
+    if r.status_code != requests.codes.ok:
+        server_error(request)
     grade_distribution = r.json()
     quiz_title = grade_distribution["quiz_title"]
     total_participants = grade_distribution['total_participants']
     quiz_id = grade_distribution["quiz_id"]
+
+    url = '/quizzes/{0}/questions'.format(quiz_id)
+    r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
+
+    questions = r.json(object_hook=json_object_hook)
+    if r.status_code != requests.codes.ok:
+        server_error(request)
     return render(request, 'courses/quiz.html', {'title':quiz_title, 'quiz_id':quiz_id,
-                                                 'total_participants': total_participants})
+                                                 'total_participants': total_participants,
+                                                 'questions':questions})
 
 def quiz_statistics(request, quiz_id):
     url='/statistics/{0}/grade_distribution'.format(quiz_id)
@@ -210,7 +229,10 @@ def quiz_statistics(request, quiz_id):
     X = []
     Y = []
     for pair in grade_distribution['grade_distribution']:
-        Y.append(pair['count']*100)
+        if pair['count']!=0:
+            Y.append(pair['count'])
+        else:
+            Y.append(0.01)
         X.append(pair['score']*100)
     ed_chart = pygal.StackedBar(legend_at_bottom=True, title_font_size=10, legend_font_size=10,
                                 style=custom_style, width=600, height=300,
@@ -225,25 +247,44 @@ def quiz_statistics(request, quiz_id):
     return response
 
 def questions_statistics(request, quiz_id):
-    url='/statistics/{0}/questions_distribution'.format(quiz_id)
+    url='/statistics/{0}/answers_distribution'.format(quiz_id)
     accept_json_header = {'accept': 'application/json'}
     email = request.session['email']
     password = request.session['password']
-    #r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
-    #questions_distribution = r.json()
-    X = []
-    Y = []
-    X = xrange(1,15)
-    Y = [x*10%6 for x in xrange(1,15)]
-    #for pair in questions_distribution['question_distribution']:
-    #    Y.append(pair['count'])
-    #    X.append(pair['score'])
-    ed_chart = pygal.StackedBar(legend_at_bottom=True, title_font_size=10, legend_font_size=10,
-                                style=question_style, width=600, height=300,
+    r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
+    answers_distribution = r.json()
+    total_submissions = answers_distribution['total_submissions']
+    correct_answers_count = []
+    wrong_answers_count = []
+    not_answered_count = []
+    num_question = 0
+    #X = xrange(1,15)
+    #Y = [x*10%6 for x in xrange(1,15)]
+    for question in answers_distribution['answers_distribution']:
+        if question['correct_answers_count'] == 0:
+            correct_answers_count.append(0.01)
+        else:
+            correct_answers_count.append(question['correct_answers_count'])
+        if question['wrong_answers_count'] == 0:
+            wrong_answers_count.append(0.01)
+        else:
+            wrong_answers_count.append(question['wrong_answers_count'])
+        if question['not_answered_count'] == 0:
+            not_answered_count.append(0.01)
+        else:
+            not_answered_count.append(question['not_answered_count'])
+        num_question = num_question+1
+
+    ed_chart = pygal.Bar(legend_at_bottom=True, title_font_size=10, legend_font_size=10,
+                                style=question_style, width=600, height=300, order_min = 0, show_x_guides =True,
                                 js= PYGAL_JS_FILES)
+    pygal.print_zeros = True
     ed_chart.title = "Per question results"
-    ed_chart.add('Number of correct answers', Y)
+    X = xrange(1,num_question+1)
     ed_chart.x_labels = [u"Q.{0}".format(x) for x in X]
+    ed_chart.add('Number of correct answers', correct_answers_count)
+    ed_chart.add('Number of not answered', not_answered_count)
+    ed_chart.add('Number of wrong answers', wrong_answers_count)
     temp = tempfile.NamedTemporaryFile('rw')
     ed_chart.render_to_file(temp.name)
     wrapper = FileWrapper(temp)
@@ -251,19 +292,35 @@ def questions_statistics(request, quiz_id):
     return response
 
 def motivation_statistics(request, quiz_id):
-    url='/statistics/{0}/motivation'.format(quiz_id)
+    url='/statistics/{0}/submissions_dates'.format(quiz_id)
     accept_json_header = {'accept': 'application/json'}
     email = request.session['email']
     password = request.session['password']
-    #r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
-    #questions_distribution = r.json()
+    r = requests.get(REST_API+url, headers=accept_json_header, auth=(email, password))
+    distribution = r.json()
+    date_published = distribution['date_published']
+    date_published = datetime.strptime( date_published[:-4], "%Y-%m-%dT%H:%M:%S." )
+    middle=0
+    short=0
+    long=0
+    for submission in distribution['submissions']:
+        date = submission['date_submitted']
+        date = datetime.strptime( date[:-4], "%Y-%m-%dT%H:%M:%S." )
+        difference = date.date()-date_published.date()
+        delta= abs(difference.days)
+        if delta <= 1:
+            short = short+1
+        elif delta <=3:
+            middle = middle+1
+        else:
+            long = long+1
     pie_chart = pygal.Pie(legend_at_bottom=True, title_font_size=10, legend_font_size=10,
                                 style=motivation_style, width=600, height=300,
                                 js= PYGAL_JS_FILES)
     pie_chart.title = 'Motivation (in %)'
-    pie_chart.add('1< day <3', 36.6)
-    pie_chart.add('< 1 day', 19.5)
-    pie_chart.add('In the last day', 36.3)
+    pie_chart.add('1< day <3', middle)
+    pie_chart.add('< 1 day', short)
+    pie_chart.add('In the last day', long)
 
     temp = tempfile.NamedTemporaryFile('rw')
     pie_chart.render_to_file(temp.name)
